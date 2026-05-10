681449ed1bfea2dba4f1e023aedd70dd6c99ea11
Alok Prasad <alokprasad573@outlook.com>
2026-05-10 15:12:16 +0530

    Re-Implementation of TeacherAuthFlow.

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
+from src.screens.home_screen import home_screen
+from src.screens.student_screen import student_screen
+from src.screens.teacher_screen import teacher_screen
 import streamlit as st
 
 def main():
-    print(1)
+    st.set_page_config(
+        page_title='SnapClass - Making Attendance faster using AI',
+        page_icon= "https://i.ibb.co/YTYGn5qV/logo.png"
+    )
+    if 'role' not in st.session_state:
+        st.session_state['role'] = None
     
+    if st.session_state['role'] == 'teacher':
+        teacher_screen()
+    elif st.session_state['role'] == 'student':
+        student_screen()
+    else:
+        home_screen()
     
-main
+
+if __name__ == "__main__":
+    main()
+    
